91ade23fe03a24e5eca790422a7ee6424de640ce
hwet <ghlckd5424@gmail.com>
2023-07-13 17:51:49 +0900

    # 회원가입 관련 정보 테스트 완료, 가입하기 버튼으로 전송완료

--- B_Project/WebContent/view/member/checkDuplicate.jsp
+++ B_Project/WebContent/view/member/checkDuplicate.jsp
@@ -1,20 +1,28 @@
-<%-- 
-<%@page import="user.MyUserDAO"%>
+<%@page import="member.model.MemberDAO"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
     
     
 <%
-String username = request.getParameter("userid");
+String userid = request.getParameter("userid");
+String username = request.getParameter("username");
+String usertlno = request.getParameter("tlno");
 String message = "";
 
-MyUserDAO userDAO = new MyUserDAO();
-boolean isDuplicate = userDAO.isUsernameDuplicate(username);
-
-if (isDuplicate) {
-    message = "duplicate";
+MemberDAO memDAO = new MemberDAO();
+ 
+if (memDAO.idDuplicateCheck(userid)) {
+    message = "id_duplicate";
 } else {
-    message = "not_duplicate";
+	if (memDAO.nicknameDuplicateCheck(username)){
+		message = "nickname_duplicate";
+	} else {
+		if (memDAO.tlnoDuplicateCheck(usertlno)){
+			message = "tlno_duplicate";
+		} else {
+			message = "not_duplicate";
+		}
+	}
 }
  
 // 클라이언트에게 응답 전송
@@ -22,4 +30,4 @@ response.setContentType("text/plain");
 response.setCharacterEncoding("UTF-8");
 // 개행 (줄바꿈) 데이터가 함께 전송되는 문제가 발생함 -> ajax에서 해결함
 response.getWriter().write(message);
-%> --%>
+%> 
--- B_Project/src/member/model/MemberDAO.java
+++ B_Project/src/member/model/MemberDAO.java
@@ -8,10 +8,12 @@ import java.sql.Timestamp;
 import java.util.Date;
 
 import jdbc.JDBCUtil;
+import jdbc.connection.ConnectionProvider;
 import member.model.MemberDTO;
 
 public class MemberDAO {
 	
+	// user_info 테이블에 정보 넣기 (회원가입) - > 관리자도 함께 사용가능할듯
 	public int joinInsert(Connection conn, MemberDTO mem) throws SQLException {
 		// 객체준비
 		String sql = "INSERT INTO user_info "
@@ -29,6 +31,84 @@ public class MemberDAO {
 		
 		// 쿼리실행
 		int result = pstmt.executeUpdate();
+		
+		return result;
+	}
+	
+	// userid가 존재하는지 확인
+	public boolean idDuplicateCheck(String userId) throws SQLException {
+		// 객체준비
+		Connection conn = ConnectionProvider.getConnection();
+		boolean result = false;
+		int cnt = 0;
+		String sql = "SELECT COUNT(*) FROM user_info WHERE user_id = ?";
+		
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		
+		pstmt.setString(1,userId);
+		
+		ResultSet rs = pstmt.executeQuery();
+		
+		while(rs.next()) { 
+			cnt = rs.getInt(1);
+		}
+		if (cnt == 0) {
+			result = false;
+		} else {
+			result = true;
+		}
+		
+		return result;
+	}
+	
+	// nickname이 존재하는지 확인
+	public boolean nicknameDuplicateCheck(String user_nickname) throws SQLException {
+		// 객체준비
+		Connection conn = ConnectionProvider.getConnection();
+		boolean result = false;
+		int cnt = 0;
+		String sql = "SELECT COUNT(*) FROM user_info WHERE user_nickname = ?";
+		
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		
+		pstmt.setString(1,user_nickname);
+		
+		ResultSet rs = pstmt.executeQuery();
+		
+		while(rs.next()) { 
+			cnt = rs.getInt(1);
+		}
+		if (cnt == 0) {
+			result = false;
+		} else {
+			result = true;
+		}
+		
+		return result;
+	}
+	
+	// 전화번호가 존재하는지 확인
+	public boolean tlnoDuplicateCheck(String user_tlno) throws SQLException {
+		// 객체준비
+		Connection conn = ConnectionProvider.getConnection();
+		boolean result = false;
+		int cnt = 0;
+		String sql = "SELECT COUNT(*) FROM user_info WHERE user_tlno = ?";
+		
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		
+		pstmt.setString(1,user_tlno);
+		
+		ResultSet rs = pstmt.executeQuery();
+		
+		while(rs.next()) { 
+			cnt = rs.getInt(1);
+		}
+		if (cnt == 0) {
+			result = false;
+		} else {
+			result = true;
+		}
 		
 		return result;
 	}
