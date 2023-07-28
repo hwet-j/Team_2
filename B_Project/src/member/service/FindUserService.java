a9787c8698cb9eb2a324eb3c238aa8b1ebf44416
ghlck <ghlckd5424@gmail.com>
2023-07-28 12:44:11 +0900

    # 회원아이디, 비밀번호 찾기 구현완료(인증번호를 받아 사용하지만, 실제 핸드폰에서 받지않음)

--- B_Project/src/member/service/FindUserService.java
+++ B_Project/src/member/service/FindUserService.java
@@ -1,6 +1,7 @@
 package member.service;
 
 import member.model.MemberDAO;
+import member.model.MemberDTO;
 
 public class FindUserService {
 	private MemberDAO  memberDAO = new MemberDAO();
@@ -9,6 +10,13 @@ public class FindUserService {
 	public boolean existTlnoCheck(String user_tlno) {
 		boolean result = false;
 		result = memberDAO.tlnoDuplicateCheck(user_tlno);
+		
+		return result;
+	}
+	
+	/* 아이디, 비밀번호 찾기에 사용될 메서드 - 전화번호로 유저 정보 가져오기 */
+	public MemberDTO findUserInfo(String user_tlno) {
+		MemberDTO result = memberDAO.findUserInfo(user_tlno);
 		
 		return result;
 	}
